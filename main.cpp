f113bfb870fa95998ddb630ffbfd4f98e5e65bca
Xue Haonan <xuehaonan27@gmail.com>
2026-01-13 04:22:38 +0800

    feat: project structure

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#include <vulkan/vulkan_raii.hpp>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <stdexcept>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+ public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+ private:
+  GLFWwindow *window;
+
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "vkraytracing", nullptr, nullptr);
+  }
+
+  void initVulkan() {}
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+};
+
+int main() {
+  HelloTriangleApplication app;
+
+  std::cout << glm::asin(1.0) << std::endl;
+
+  try {
+    app.run();
+  }
+  catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
